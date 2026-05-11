8eb41fac2f411142d43448c0ce1b8dfa197e0480
Erisvaldo <erisvaldoapps@gmail.com>
2026-05-10 21:00:04 -0300

    update:4

--- src/app/components/checkout-component/checkout-component.ts
+++ src/app/components/checkout-component/checkout-component.ts
@@ -1,12 +1,12 @@
 import { Component, computed, signal } from '@angular/core';
 import { TicketItemComponent } from '../ticket-item-component/ticket-item-component';
 import { CardComponent } from '../card-component/card-component';
-import { CurrencyPipe, DatePipe } from '@angular/common';
+import { CurrencyPipe, DatePipe, NgClass } from '@angular/common';
 import { ResumoPipe } from '../../resumo-pipe';
 
 @Component({
   selector: 'app-checkout-component',
-  imports: [TicketItemComponent, CardComponent, CurrencyPipe, DatePipe, ResumoPipe],
+  imports: [TicketItemComponent, CardComponent, CurrencyPipe, DatePipe, ResumoPipe, NgClass],
   templateUrl: './checkout-component.html',
   styleUrl: './checkout-component.css',
 })
@@ -15,9 +15,9 @@ export class CheckoutComponent {
   mensagemStatus = signal("Aguardando finalização...");
   total = computed(() => this.ingressos().reduce((acc, ingresso) => acc + ingresso.preco, 0));
   ingressos = signal([
-    { id: 1, nome: 'Ingresso 1', tipo: 'VIP', data: new Date('2026-04-17T18:09:38'), preco: 200, descricao: 'Acesso a área VIP do evento' },
-    { id: 2, nome: 'Ingresso 2', tipo: 'MEIA', data: new Date('2026-04-17T08:09:38'), preco: 50, descricao: 'Nenhum' },
-    { id: 3, nome: 'Ingresso 3', tipo: 'STANDARD', data: new Date('2026-04-17T19:09:38'), preco: 100, descricao: 'Nenhum' },
+    { id: 1, nome: 'Ingresso 1', tipo: 'VIP', data: new Date('2026-04-17T18:09:38'), preco: 200, descricao: 'Acesso a área VIP do evento', lotePercentual: 80 },
+    { id: 2, nome: 'Ingresso 2', tipo: 'MEIA', data: new Date('2026-04-17T08:09:38'), preco: 50, descricao: 'Nenhum', lotePercentual: 15 },
+    { id: 3, nome: 'Ingresso 3', tipo: 'STANDARD', data: new Date('2026-04-17T19:09:38'), preco: 100, descricao: 'Nenhum', lotePercentual: 50 },
   ]);
   qtd = signal(this.ingressos().length);
 
@@ -25,6 +25,16 @@ export class CheckoutComponent {
   notificarUsuario() {
     this.mensagemStatus.set("O usuário solicitou o cancelamento da compra!");
     this.qtd.set(1);
+  }
+
+  corDaBarra(lotePercentual: number): string {
+    if (lotePercentual < 20) {
+      return 'red';
+    } else if (lotePercentual <= 50) {
+      return 'yellow';
+    } else {
+      return 'green';
+    }
   }
 
 }
